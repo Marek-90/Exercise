1f7af11e7643c688bcc1983e6e3bbcf7431b871b
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-08-31 13:28:56 +0200

    reduction

--- src/Click/Click.jsx
+++ src/Click/Click.jsx
@@ -7,7 +7,8 @@ const Click = () => {
   const [result, setResult] = useState(0);
 
   const handleMathClick = (type, number = 1) => {
-    if (type === "substraction") {
+    // debugger;
+    if (type === "subtraction") {
       setCount((prevState) => prevState + 1);
       setResult((prevState) => prevState - number);
     } else if (type === "reset") {
@@ -21,37 +22,52 @@ const Click = () => {
 
   return (
     <>
-      <button
-        onClick={() => handleMathClick("substraction", 10)}
-        className="btn__click"
-      >
-        -10
-      </button>
-      <button
-        onClick={() => handleMathClick("substraction")}
-        className="btn__click"
-      >
-        -1
-      </button>
-      <button onClick={() => handleMathClick("reset")} className="btn__click">
-        Reset
-      </button>
-      <button
-        onClick={() => handleMathClick("addition")}
-        className="btn__click"
-      >
-        +1
-      </button>
-      <button
-        onClick={() => handleMathClick("addition", 10)}
-        className="btn__click"
-      >
-        +10
-      </button>
+      <MathButton
+        name="-10"
+        number="10"
+        type="subtraction"
+        click={handleMathClick}
+      />
       <h2>Liczba kliknięć: {count}</h2>
       <h2>Wynik: {result}</h2>
     </>
   );
 };
 
+const MathButton = (props) => {
+  return (
+    <button onClick={() => props.click(props.type, props.number)}>
+      {props.name}
+    </button>
+  );
+};
+
 export default Click;
+
+// <button
+//         onClick={() => handleMathClick("subtraction", 10)}
+//         className="btn__click"
+//       >
+//         -10
+//       </button>
+//       <button
+//         onClick={() => handleMathClick("subtraction")}
+//         className="btn__click"
+//       >
+//         -1
+//       </button>
+//       <button onClick={() => handleMathClick("reset")} className="btn__click">
+//         Reset
+//       </button>
+//       <button
+//         onClick={() => handleMathClick("addition")}
+//         className="btn__click"
+//       >
+//         +1
+//       </button>
+//       <button
+//         onClick={() => handleMathClick("addition", 10)}
+//         className="btn__click"
+//       >
+//         +10
+//       </button>
